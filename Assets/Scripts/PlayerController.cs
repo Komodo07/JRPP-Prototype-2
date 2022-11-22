16ea2a18e4f1fe7d28653260a1ecdeba34747dc5
Jason Tucker <komodo.07@outlook.com>
2022-11-22 11:01:24 -0600

    added player movement
    added player movement on the x axis and blocked player from moving outside of bounds

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 12.0f;
+    private float currentPositionX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InBounds(); //Keeps the player in the bounds of the play area
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+
+    void InBounds()
+    {
+        currentPositionX = transform.position.x;
+
+        if (currentPositionX < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (currentPositionX > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
