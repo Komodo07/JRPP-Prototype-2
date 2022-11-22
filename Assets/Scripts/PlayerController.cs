905801df3c6d5ad90ec7771c0e23d2fd771f610c
Jason Tucker <komodo.07@outlook.com>
2022-11-22 16:43:14 -0600

    added fire projectile method
    began adding fire projectile method to player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float xRange = 12.0f;
     private float currentPositionX;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         InBounds(); //Keeps the player in the bounds of the play area
+        FireProjectile(); //Instantiates a projectile from players location
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
@@ -35,6 +38,14 @@ public class PlayerController : MonoBehaviour
         if (currentPositionX > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+
+    void FireProjectile()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch projectile
         }
     }
 }
